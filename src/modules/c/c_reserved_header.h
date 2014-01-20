1e197a2a205571bee5e006c687aaef4d1936ad07
Joshua Rahm <joshua.rahm@colorado.edu>
2014-01-19 21:38:50 -0700

    added missing header file

---
+++ src/modules/c/c_reserved_header.h
@@ -0,0 +1,11 @@
+const char* c_reserved_words[] = {
+"auto", "else", "long", "switch",
+"break", "enum", "register", "typedef",
+"case", "extern", "return", "union",
+"char", "float", "short", "unsigned",
+"const", "for", "signed", "void",
+"continue", "goto", "sizeof", "volatile",
+"default", "if", "static", "while",
+"do", "int", "struct",
+"double", "__attribute__", NULL /* must end with NULL */
+};
